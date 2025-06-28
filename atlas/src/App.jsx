45b7b1831f2c6057b51049c96a1ee97a80e1b93e
bhoomigundecha <bhoomigundecha@gmail.com>
2025-06-28 11:32:26 +0530

    done

--- atlas/src/App.jsx
+++ atlas/src/App.jsx
@@ -1,5 +1,34 @@
+import './App.css';
+import { CreateBrowserRouter, RouterProvider } from "react-router-dom";
+
+import {About} from './pages/About';
+import {Home} from './pages/Home';
+import {Country} from './pages/Country';
+import {Contact} from './pages/Contact';
+
+const router = CreateBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "country",
+    element: <Country />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+])
+
+
+
 const App = () => {
-  return <h1> Hello React Porject </h1>
+  return <RouterProvider> </RouterProvider>
 }
 
 export default App;
